556a1c6d3f02a452387e7e7bd504584c0f078913
Maison-A <maisonarroyo@protonmail.com>
2023-06-15 14:41:38 -0400

    sound for boost, success and fail are implemented

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // Parameters - tuning, editor vars
+    // Cache - references for readability
+    // State - private members
+
     // making move speed public to be serialized
     [SerializeField] float mainThrust = 1000f;
     [SerializeField] float rotateThrust = 100f;
+    [SerializeField] AudioClip engineThrust;
+    
     Rigidbody rb = null;
     AudioSource asSource = null;
     
     // Start is called before the first frame update
     void Start()
     {
-        // get reference to component rigidbody and set to rb
-        rb = GetComponent<Rigidbody>();
-        // get audio source
-        asSource = GetComponent<AudioSource>();
+        
+        rb = GetComponent<Rigidbody>(); // get reference to component rigidbody and set to rb
+        asSource = GetComponent<AudioSource>(); // get audio source
     }
 
     // Update is called once per frame
@@ -25,12 +30,20 @@ public class PlayerMovement : MonoBehaviour
         ProcessInput();
     }
 
+    /*
+     * Name: ProcessInput
+     * Desc: call methods to handle user input for rocket
+     */
     void ProcessInput()
     {
         ProcessThrust();
         ProcessRotation();
     }
 
+    /*
+     * Name: ProcessThrust
+     * Desc: method to take input and calculate thrust on rocket
+     */
     void ProcessThrust()
     {
         // GetKey will return true while input is held
@@ -39,7 +52,7 @@ public class PlayerMovement : MonoBehaviour
         {
             if (!asSource.isPlaying)
             {
-                asSource.Play();
+                asSource.PlayOneShot(engineThrust);
             }
             //vector is direction and magnitude
             rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
@@ -49,7 +62,10 @@ public class PlayerMovement : MonoBehaviour
             asSource.Stop();
         }
     }
-
+    /*
+     * Name: ProcessRotation
+     * Desc: handle player rotate information
+     */
     void ProcessRotation()
     {
         if (Input.GetKey(KeyCode.A))
@@ -63,7 +79,11 @@ public class PlayerMovement : MonoBehaviour
         }
                 
     }
-
+    /*
+     * Name: ApplyRotate
+     * Desc: disallow engine physics to compete with user input and calculate 
+     * frame independent rotation
+     */
     void ApplyRotate(float rotationThisFrame)
     {
         
--- Assets/Scripts/World/Collisions/CollisionHandler.cs
+++ Assets/Scripts/World/Collisions/CollisionHandler.cs
@@ -2,32 +2,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using System;
 
 public class CollisionHandler : MonoBehaviour
 {
-    [SerializeField] float reloadDelay = 2f;
-    // don't need start and update for this script
+    [SerializeField] float loadDelay = 2f;
+    [SerializeField] AudioClip soundCrash;
+    [SerializeField] AudioClip soundSuccess;
+    AudioSource asSource = null;
+    bool isTransitioning = false;
+
+    private void Start()
+    {
+        asSource = GetComponent<AudioSource>();
+    }
+
     private void OnCollisionEnter(Collision other)
     {
+        if (isTransitioning) {return; }// break out if is Transitioning is set to true
+
         switch (other.gameObject.tag)
         {
-            case "Friendly":
-                Debug.Log("Friendly");
-                break;
             case "Finish":
-                LoadNextScene();
+                SuccessSequence();
                 break;
             default:
-                // invoke allows for delay of method call
-                EndLevelProcess();
+                CrashSequence();
                 break;
         }
     }
-    void EndLevelProcess()
+
+    /*
+     * Name: CrashSequence
+     * Desc: sequence of methods to be called & logic to be done when "crash" state is invoked after
+     * player fails to reach goal - similar logic will be used in SuccessSequence()
+     */
+    void CrashSequence()
     {
-        GetComponent<PlayerMovement>().enabled = false;
-        Invoke("SceneReload", reloadDelay);
+        isTransitioning = true; // set transition state to true
+        asSource.Stop(); // stop the source to prevent overlap
+        asSource.PlayOneShot(soundCrash); // play sound set in enviornment editor
+
+        GetComponent<PlayerMovement>().enabled = false; // disable player movement
+        Invoke("SceneReload", loadDelay); // invoke allows for delay of method call
     }
+
+    /*
+     * Name: SuccessSequence
+     * Desc: sequence of methods to be called & logic to be done when "success" state is invoked
+     * after player reaches goal
+     */
+    void SuccessSequence()
+    {
+        isTransitioning = true;
+        asSource.Stop();
+        asSource.PlayOneShot(soundSuccess);
+        
+        GetComponent<PlayerMovement>().enabled = false;
+        Invoke("LoadNextScene", loadDelay);
+    }
+
+    /*
+     * Name: SceneReload
+     * Desc: reload current scene by passing in active scene index
+     */
     void SceneReload()
     {
         // get current scene and set it as the scene to be reloaded
@@ -35,6 +73,10 @@ public class CollisionHandler : MonoBehaviour
         SceneManager.LoadScene(currentSceneIndex);
     }
 
+    /*
+     * Name: LoadNextScene
+     * Desc: load next scene after player "wins" current scene
+     */
     void LoadNextScene()
     {
         // get current scene and set it as the scene to be reloaded
