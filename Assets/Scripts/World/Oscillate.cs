3625a9c845235167d9e1e52302dc9c6506b725fc
Maison-A <maisonarroyo@protonmail.com>
2023-06-20 13:23:49 -0400

    oscillate inplemented

---
+++ Assets/Scripts/World/Oscillate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Oscillate : MonoBehaviour
+{
+    Vector3 StartingPos;
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] [Range(0,1)] float movementFactor;
+
+    void Start()
+    {
+        StartingPos = transform.position;
+        
+    }
+
+    void Update()
+    {
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = StartingPos + offset;
+    }
+}
