dee46dfee604d93cc08a7c49d373b6d4bba9d6f7
Maison-A <maisonarroyo@protonmail.com>
2023-06-20 14:16:43 -0400

    oscillate optimized

--- Assets/Scripts/World/Oscillate.cs
+++ Assets/Scripts/World/Oscillate.cs
@@ -9,7 +9,8 @@ public class Oscillate : MonoBehaviour
 {
     Vector3 StartingPos;
     [SerializeField] Vector3 movementVector;
-    [SerializeField] [Range(0,1)] float movementFactor;
+    float movementFactor;
+    [SerializeField] float period = 2f;
 
     void Start()
     {
@@ -19,6 +20,14 @@ public class Oscillate : MonoBehaviour
 
     void Update()
     {
+        // Epsilon is the smallest float - we must use that instead
+        // since 0f can be a different number
+        if (period <= Mathf.Epsilon) { return; }
+        float cycles = Time.time / period; // continues to grow over time
+        const float tau = Mathf.PI*2; // const val of 6.283..
+        
+        float rawSinWave = Mathf.Sin(cycles * tau);// should give us a value of -1 to 1
+        movementFactor = (rawSinWave + 1f)/ 2f; // recalculated to go from 0 -> 1
         Vector3 offset = movementVector * movementFactor;
         transform.position = StartingPos + offset;
     }
